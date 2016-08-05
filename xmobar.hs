5fb7b611043bae60c4dc17a013a5269cb7024ef7
Aswin <techno.aswin@gmail.com>
2016-08-05 18:09:40 +0530

    Volume on xmobad. Also go solarized

--- xmobar.hs
+++ xmobar.hs
@@ -3,19 +3,20 @@
 
 Config {
     font = "xft:Fixed-8",
-    bgColor = "#000000",
-    fgColor = "#ffffff",
+    bgColor = "#002b36",
+    fgColor = "#93a1a1",
     position = Static { xpos = 0, ypos = 0, width = 1820, height = 16 },
-    lowerOnStart = True,
+    lowerOnStart = False,
     commands = [
         Run MultiCpu ["-t","Cpu: <total0> <total1> <total2> <total3>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
         Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run Network "wlp7s0" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run Date "%a %b %_d %l:%M" "date" 10,
+        Run Com "/home/aswin/bin/volume.sh" [] "myVolume" 10,
         Run StdinReader
     ],
     sepChar = "%",
     alignSep = "}{",
-    template = "%StdinReader% }{ %multicpu%   %memory%   %swap%   %wlp7s0%   <fc=#FFFFCC>%date% </fc>"
+    template = "%StdinReader% }{ %multicpu%   %memory%   %swap%  Vol: %myVolume% %wlp7s0%   <fc=#FFFFCC>%date% </fc>"
 }
